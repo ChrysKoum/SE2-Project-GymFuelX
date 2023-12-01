ee843675547a64fd2003ad16b692cfda9d236b1c
Chrysostomos Koumides <chyrkoum@gmail.com>
2023-12-01 13:38:12 +0200

    fix the first test of dietprogrammUser and add the exerciseUser test

--- tests/DietprogramUser.test.js
+++ tests/DietprogramUser.test.js
@@ -6,6 +6,7 @@ const app = require("../index.js");
 const { getDietProgram } = require("../service/DietprogramUserService");
 
 const userID = generateTestUserID(); // Example of a user ID
+const maxUserID = Math.pow(10, 8); // Define the maximum allowable userID
 
 test.before(async (t) => {
   t.context.server = http.createServer(app);
@@ -21,18 +22,38 @@ test.after.always((t) => {
 });
 
 test("getDietProgram returns the correct structure for a valid user ID", async (t) => {
-  const response = await getDietProgram(userID);
+  const result = await getDietProgram(userID);
 
-  t.truthy(
-    response.dayDietProgramIDs,
-    "Response should have dayDietProgramIDs"
-  );
-  t.truthy(response.recipeIDs, "Response should have recipeIDs");
+  t.truthy(result.dayDietProgramIDs, "Response should have dayDietProgramIDs");
+  t.truthy(result.recipeIDs, "Response should have recipeIDs");
   t.is(
-    typeof response.dietProgramID,
+    typeof result.dietProgramID,
     "number",
     "dietProgramID should be a number"
   );
+});
+
+test("getDietProgram returns a response with the correct structure and data types", async (t) => {
+  const userID = generateTestUserID(); // Function that generates a valid userID
+
+  const { body, statusCode } = await t.context.got.get(
+    `user/${userID}/dietprogram`
+  );
+
+  // Assuming the API returns a 200 status code for valid requests
+  t.is(statusCode, 200, "Should return 200 OK for valid userID");
+
+  // Validate the response structure
+  t.true(
+    Array.isArray(body.dayDietProgramIDs),
+    "dayDietProgramIDs should be an array"
+  );
+  t.true(
+    body.dayDietProgramIDs.every((day) => Array.isArray(day)),
+    "Each element in dayDietProgramIDs should be an array"
+  );
+  t.true(Array.isArray(body.recipeIDs), "recipeIDs should be an array");
+  t.is(typeof body.dietProgramID, "number", "dietProgramID should be a number");
 });
 
 function generateTestUserID() {
