adec007ba46d25aed91b06c10f5219516abece49
tryfkalt <116564459+tryfkalt@users.noreply.github.com>
2023-12-02 22:45:16 +0200

    modified-added more tests

--- tests/UserUser.test.js
+++ tests/UserUser.test.js
@@ -65,22 +65,51 @@ test('getUserDetails returns user details', async (t) => {
 
 test('GET user with minimum valid user ID returns correct response', async (t) => {
     const { body, statusCode } = await t.context.got('user/1');
-
     t.is(statusCode, 200, "Should return 200 OK for valid userID");
-
 });
 
 test('GET user with maximum valid user ID returns correct response', async (t) => {
     const { body, statusCode } = await t.context.got('user/1000000');
-
     t.is(statusCode, 200, "Should return 200 OK for valid userID");
-
 });
 
+const userIDfor400 = [1.2, 'abc', true, '@special', null, undefined, '!', '@', '^', '&', '*',];
 test('GET user with non-numeric user ID returns 400', async (t) => {
-    const nonNumericUserID = 'abc';
-    const { statusCode } = await t.context.got(`user/${nonNumericUserID}`);
-    t.is(statusCode, 400, 'Should return 400 Bad Request for non-numeric userID');
+    for (const userID of userIDfor400) {
+        const nonNumericUserID = userID;
+        const { body, statusCode } = await t.context.got(`user/${nonNumericUserID}`);
+        // Assertions
+        t.is(statusCode, 400, 'Should return 400 Bad Request for non-numeric userID');
+        t.assert(body.message);
+        t.is(body.message, 'request.params.userID should be integer');
+        t.deepEqual(body.errors, [
+            {
+                path: '.params.userID',
+                message: 'should be integer',
+                errorCode: 'type.openapi.validation'
+            }
+        ]);
+    }
+});
+
+const userIDfor404 = ['', []]
+test('GET user with non-numeric user ID returns 404', async (t) => {
+    for (const userID of userIDfor404) {
+        const nonNumericUserID = userID;
+
+        const { body, statusCode } = await t.context.got(`user/${nonNumericUserID}`);
+
+        // Assertions
+        t.is(statusCode, 404, 'Should return 404 User not found.');
+        t.assert(body.message);
+        t.is(body.message, 'not found');
+        t.deepEqual(body.errors, [
+            {
+                path: '/user/',
+                message: 'not found',
+            }
+        ]);
+    }
 });
 
 test('GET user returns expected headers', async (t) => {
@@ -90,3 +119,99 @@ test('GET user returns expected headers', async (t) => {
     t.is(statusCode, 200, "Should return 200 OK for valid userID");
     t.truthy(headers['content-type'], 'Response should have content-type header');
 });
+
+test('PUT editUserDetails returns success response with required fields', async (t) => {
+    const userID = 0;
+    const mockRequestBody = {
+        userID: userID,
+        username: "testName",
+        birthday: '2000-01-23T04:56:07.000+00:00',
+        gender: 'gender',
+        height: 6.027456183070403,
+        weight: 1.4658129805029452,
+        meal: "testMeal",
+        allergies: "testAllergies",
+        restrictions: "testRestrictions",
+        level: "testLevel",
+        goal: "testGoal"
+    };
+
+    const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
+        json: mockRequestBody,
+    });
+    // Assertions
+    t.is(statusCode, 200, 'Should return 200 OK for successful editUserDetails');
+    t.truthy(body.birthday);
+    t.truthy(body.gender);
+    t.truthy(body.height);
+    t.truthy(body.weight);
+});
+
+
+test('PUT editUserDetails with invalid userID returns fail response - 400 ', async (t) => {
+    for (const userID of userIDfor400) {
+        const nonNumericUserID = userID;
+        const mockRequestBody = {
+            userID: nonNumericUserID,
+            username: "testName",
+            birthday: '2000-01-23T04:56:07.000+00:00',
+            gender: 'gender',
+            height: 6.027456183070403,
+            weight: 1.4658129805029452,
+            meal: "testMeal",
+            allergies: "testAllergies",
+            restrictions: "testRestrictions",
+            level: "testLevel",
+            goal: "testGoal"
+        };
+
+        const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
+            json: mockRequestBody,
+        });
+        // Assertions
+        t.is(statusCode, 400, 'Should return 400 Bad Request for non-numeric userID');
+        t.assert(body.message);
+        t.is(body.message, 'request.params.userID should be integer');
+        t.deepEqual(body.errors, [
+            {
+                path: '.params.userID',
+                message: 'should be integer',
+                errorCode: 'type.openapi.validation'
+            }
+        ]);
+    }
+});
+
+test('PUT editUserDetails with invalid userID returns fail response 404 ', async (t) => {
+    for (const userID of userIDfor404) {
+        const nonNumericUserID = userID;
+        const mockRequestBody = {
+            userID: nonNumericUserID,
+            username: "testName",
+            birthday: '2000-01-23T04:56:07.000+00:00',
+            gender: 'gender',
+            height: 6.027456183070403,
+            weight: 1.4658129805029452,
+            meal: "testMeal",
+            allergies: "testAllergies",
+            restrictions: "testRestrictions",
+            level: "testLevel",
+            goal: "testGoal"
+        };
+
+        const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
+            json: mockRequestBody,
+        });
+        // Assertions
+        t.is(statusCode, 404, 'Should return 404 User not found.');
+        t.assert(body.message);
+        t.is(body.message, 'not found');
+        t.deepEqual(body.errors, [
+            {
+                path: '/user/',
+                message: 'not found',
+            }
+        ]);
+    }
+});
+
