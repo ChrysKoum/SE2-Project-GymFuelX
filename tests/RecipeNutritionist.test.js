6a01a83e6cee153608e13a71d3685bd51101ea43
KomisD <63542836+KomisD@users.noreply.github.com>
2023-12-05 21:56:12 +0200

    Get RecipeNutritionist testing

--- tests/RecipeNutritionist.test.js
+++ tests/RecipeNutritionist.test.js
@@ -13,7 +13,47 @@ test.before(async (t) => {
     t.context.got = got.extend({ prefixUrl: t.context.prefixUrl, responseType: 'json' });
 });
 
+const nutritionistID = generateTestnutritionistID();
+const recipeID = generateTestRecipeID();
+const maxRecipeID = Math.pow(10, 6); //the max Id a Nutritionist can have
+
+test('Test of the stracture that get gives', async (t) => {
+
+    const { body, statusCode } = await t.context.got.get(
+        `nutritionist/${nutritionistID}/recipe/${recipeID}`
+    );
+
+    //status Code should be 200
+    t.is(statusCode, 200, "Should return 200 OK for valid userID");
+
+    //Checking the Arrays
+    t.true(Array.isArray(body.IngredientsName) && Array.isArray(body.Instructions)
+     && Array.isArray(body.NutritionalTable) && Array.isArray(body.IngredientsQuantity)
+     , 'checking that all the variables that should be string are');
+
+    //Checking the strings
+    t.is(typeof body.difficulty, 'string');
+    t.is(typeof body.servings, 'string');
+    t.is(typeof body.recipeType, 'string');
+
+    //Checking the RecipeID and time
+    t.is(typeof body.recipeID, 'number');
+    t.is(typeof body.time, 'number');
+
+    console.log('my prints');
+    console.log(body);
+    console.log(typeof body.recipeID);
+})
+
+
 test.after.always((t) => {
     t.context.server.close();
 });
 
+function generateTestnutritionistID() {
+    return Math.floor(Math.random() * 100000) + 1;
+}
+
+function generateTestRecipeID() {
+    return Math.floor(Math.random() * 1000000) + 1;
+}
