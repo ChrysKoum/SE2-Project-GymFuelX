92bff20317d17d1a8011ee96b5f68d72ffa2c626
tryfkalt <116564459+tryfkalt@users.noreply.github.com>
2023-12-06 15:50:57 +0200

    some fixes tets/ need to implement to the rest

--- tests/RecipeNutritionist.test.js
+++ tests/RecipeNutritionist.test.js
@@ -29,8 +29,8 @@ test('Test of the stracture that get gives', async (t) => {
 
     //Checking the Arrays
     t.true(Array.isArray(body.IngredientsName) && Array.isArray(body.Instructions)
-     && Array.isArray(body.NutritionalTable) && Array.isArray(body.IngredientsQuantity)
-     , 'checking that all the variables that should be string are');
+        && Array.isArray(body.NutritionalTable) && Array.isArray(body.IngredientsQuantity)
+        , 'checking that all the variables that should be string are');
 
     //Checking the strings
     t.is(typeof body.difficulty, 'string');
@@ -45,48 +45,48 @@ test('Test of the stracture that get gives', async (t) => {
 
 //Put Request
 test('PUT RecipeNutritionist returns corect response with required fields', async (t) => {
-    
+
     const mockRecipeData = {
         IngredientsName: ["Chicken Breast", "Olive Oil", "Garlic", "Lemon Juice", "Paprika", "Salt", "Black Pepper", "Fresh Parsley"],
         difficulty: "Easy",
         servings: "4",
         recipeType: "Main Course",
         Instructions: [
-          "Preheat oven to 375°F (190°C).",
-          "In a bowl, mix olive oil, minced garlic, lemon juice, paprika, salt, and black pepper.",
-          "Place chicken breasts in a baking dish and pour the mixture over them.",
-          "Bake in the preheated oven for 25-30 minutes or until chicken is cooked through.",
-          "Garnish with chopped fresh parsley before serving."
+            "Preheat oven to 375°F (190°C).",
+            "In a bowl, mix olive oil, minced garlic, lemon juice, paprika, salt, and black pepper.",
+            "Place chicken breasts in a baking dish and pour the mixture over them.",
+            "Bake in the preheated oven for 25-30 minutes or until chicken is cooked through.",
+            "Garnish with chopped fresh parsley before serving."
         ],
         NutritionalTable: [
-          "Calories: 165",
-          "Protein: 26g",
-          "Fat: 4.5g",
-          "Carbohydrates: 3g",
-          "Sodium: 322mg"
+            "Calories: 165",
+            "Protein: 26g",
+            "Fat: 4.5g",
+            "Carbohydrates: 3g",
+            "Sodium: 322mg"
         ],
         IngredientsQuantity: [
-          "4 medium-sized chicken breasts",
-          "2 tablespoons olive oil",
-          "3 cloves garlic, minced",
-          "2 tablespoons lemon juice",
-          "1 teaspoon paprika",
-          "1/2 teaspoon salt",
-          "1/4 teaspoon black pepper",
-          "2 tablespoons chopped fresh parsley"
+            "4 medium-sized chicken breasts",
+            "2 tablespoons olive oil",
+            "3 cloves garlic, minced",
+            "2 tablespoons lemon juice",
+            "1 teaspoon paprika",
+            "1/2 teaspoon salt",
+            "1/4 teaspoon black pepper",
+            "2 tablespoons chopped fresh parsley"
         ],
         time: 35,
         recipeID: 101,
         imgRecipe: "https://example.com/images/chicken-breast-recipe.jpg"
-      };
-      
+    };
+
     const { body, statusCode } = await t.context.got.put(
         `nutritionist/${nutritionistID}/recipe/${recipeID}`, {
         json: mockRecipeData,
     });
     // Assertions
     t.is(statusCode, 200, 'Should return 200 ');
-    
+
     console.log(body);
 });
 
@@ -99,52 +99,65 @@ test('PUT RecipeNutritionist returns error 400 with bad parameters', async (t) =
         servings: "invalid_number", // Intentionally incorrect type for testing
         recipeType: "Main Course",
         Instructions: [
-          "Preheat oven to 375°F (190°C).",
-          "In a bowl, mix olive oil, minced garlic, lemon juice, paprika, salt, and black pepper.",
-          "Place chicken breasts in a baking dish and pour the mixture over them.",
-          "Bake in the preheated oven for 25-30 minutes or until chicken is cooked through.",
-          "Garnish with chopped fresh parsley before serving."
+            "Preheat oven to 375°F (190°C).",
+            "In a bowl, mix olive oil, minced garlic, lemon juice, paprika, salt, and black pepper.",
+            "Place chicken breasts in a baking dish and pour the mixture over them.",
+            "Bake in the preheated oven for 25-30 minutes or until chicken is cooked through.",
+            "Garnish with chopped fresh parsley before serving."
         ],
         NutritionalTable: [
-          "Calories: 165",
-          "Protein: 26g",
-          "Fat: 4.5g",
-          "Carbohydrates: 3g",
-          "Sodium: 322mg"
+            "Calories: 165",
+            "Protein: 26g",
+            "Fat: 4.5g",
+            "Carbohydrates: 3g",
+            "Sodium: 322mg"
         ],
         IngredientsQuantity: [
-          "4 medium-sized chicken breasts",
-          "2 tablespoons olive oil",
-          "3 cloves garlic, minced",
-          "2 tablespoons lemon juice",
-          "1 teaspoon paprika",
-          "1/2 teaspoon salt",
-          "1/4 teaspoon black pepper",
-          "2 tablespoons chopped fresh parsley"
+            "4 medium-sized chicken breasts",
+            "2 tablespoons olive oil",
+            "3 cloves garlic, minced",
+            "2 tablespoons lemon juice",
+            "1 teaspoon paprika",
+            "1/2 teaspoon salt",
+            "1/4 teaspoon black pepper",
+            "2 tablespoons chopped fresh parsley"
         ],
         time: "5", // Assuming this should be a number
         recipeID: 101,
         imgRecipe: "https://example.com/images/chicken-breast-recipe.jpg"
     };
 
-    try {
-        const { body, statusCode } = await t.context.got.put(
-            `nutritionist/${nutritionistID}/recipe/${recipeID}`, {
-            json: mockRecipeData,
-        });
+    const { body, statusCode } = await t.context.got.put(
+        `nutritionist/${nutritionistID}/recipe/${recipeID}`, {
+        json: mockRecipeData,
+    });
+    t.is(statusCode, 400, 'Should return 400 Bad Request for non-numeric userID');
+    t.is(body.message, 'request.body.time should be integer');
+    t.like(body.errors, [
+        {
+            path: '.body.time',
+            message: 'should be integer',
+            errorCode: 'type.openapi.validation'
+        }
+    ]);
+    // try {
+    //     const { body, statusCode } = await t.context.got.put(
+    //         `nutritionist/${nutritionistID}/recipe/${recipeID}`, {
+    //         json: mockRecipeData,
+    //     });
 
-        
-    } catch (error) {
-        // Assert that the error status code is 400
-        t.is(error.response.statusCode, 400, 'Should return 400 with bad parameters');
-        t.like(error.response.body.errors,[
-            {
-              path: '.body.time',
-              message: 'should be integer',
-              errorCode: 'type.openapi.validation'
-            }
-          ]);
-    }
+
+    // } catch (error) {
+    //     // Assert that the error status code is 400
+    //     t.is(error.response.statusCode, 400, 'Should return 400 with bad parameters');
+    //     t.like(error.response.body.errors, [
+    //         {
+    //             path: '.body.time',
+    //             message: 'should be integer',
+    //             errorCode: 'type.openapi.validation'
+    //         }
+    //     ]);
+    // }
 });
 
 //Delete Recipe
@@ -169,15 +182,15 @@ test('Test of the Delete Recipe with 400 error code', async (t) => {
 
     } catch (error) {
         t.is(error.response.statusCode, 400, "Should return 400 when there is a bad parameter");
-        t.like(error.response.body.errors,[
+        t.like(error.response.body.errors, [
             {
                 path: '.params.recipeID',
                 message: 'should be integer',
                 errorCode: 'type.openapi.validation'
             }
-          ]);
+        ]);
     }
-    
+
 
 });
 
