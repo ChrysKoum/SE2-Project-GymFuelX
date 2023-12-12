60cc29c87675aceaecb031e8665a4585b33072c1
Dadopoulos <71187766+MDadopoulos@users.noreply.github.com>
2023-12-12 06:28:25 +0200

    User POST addUserDetails

--- tests/UserUser.test.js
+++ tests/UserUser.test.js
@@ -118,22 +118,22 @@ test('GET user returns expected headers', async (t) => {
     t.is(statusCode, 200, "Should return 200 OK for valid userID");
     t.truthy(headers['content-type'], 'Response should have content-type header');
 });
-
+const mockRequestBody = {
+    userID: 12,
+    username: "testName",
+    birthday: '2000-01-23T04:56:07.000+00:00',
+    gender: 'gender',
+    height: 6.027456183070403,
+    weight: 1.4658129805029452,
+    meal: "testMeal",
+    allergies: "testAllergies",
+    restrictions: "testRestrictions",
+    level: "testLevel",
+    goal: "testGoal"
+};
 test('PUT editUserDetails returns success response with required fields', async (t) => {
     const userID = 0;
-    const mockRequestBody = {
-        userID: userID,
-        username: "testName",
-        birthday: '2000-01-23T04:56:07.000+00:00',
-        gender: 'gender',
-        height: 6.027456183070403,
-        weight: 1.4658129805029452,
-        meal: "testMeal",
-        allergies: "testAllergies",
-        restrictions: "testRestrictions",
-        level: "testLevel",
-        goal: "testGoal"
-    };
+    mockRequestBody.userID = userID;
 
     const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
         json: mockRequestBody,
@@ -149,19 +149,7 @@ test('PUT editUserDetails returns success response with required fields', async 
 test('PUT editUserDetails with invalid userID returns fail response - 400 ', async (t) => {
     for (const userID of userIDfor400) {
         const nonNumericUserID = userID;
-        const mockRequestBody = {
-            userID: nonNumericUserID,
-            username: "testName",
-            birthday: '2000-01-23T04:56:07.000+00:00',
-            gender: 'gender',
-            height: 6.027456183070403,
-            weight: 1.4658129805029452,
-            meal: "testMeal",
-            allergies: "testAllergies",
-            restrictions: "testRestrictions",
-            level: "testLevel",
-            goal: "testGoal"
-        };
+        mockRequestBody.userID = nonNumericUserID;
 
         const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
             json: mockRequestBody,
@@ -183,19 +171,7 @@ test('PUT editUserDetails with invalid userID returns fail response - 400 ', asy
 test('PUT editUserDetails with invalid userID returns fail response 404 ', async (t) => {
     for (const userID of userIDfor405) {
         const nonNumericUserID = userID;
-        const mockRequestBody = {
-            userID: nonNumericUserID,
-            username: "testName",
-            birthday: '2000-01-23T04:56:07.000+00:00',
-            gender: 'gender',
-            height: 6.027456183070403,
-            weight: 1.4658129805029452,
-            meal: "testMeal",
-            allergies: "testAllergies",
-            restrictions: "testRestrictions",
-            level: "testLevel",
-            goal: "testGoal"
-        };
+        mockRequestBody.userID = nonNumericUserID;
 
         const { body, statusCode } = await t.context.got.put(`user/${userID}`, {
             json: mockRequestBody,
@@ -246,37 +222,102 @@ test('editUserDetails successfully edits user details', async (t) => {
     });
 });
 
+test('POST  addUserDetails returns success response with required fields', async (t) => {
+    const newUserID = 1456;
+    mockRequestBody.userID = newUserID;
+    const { body, statusCode } = await t.context.got.post(`user/`, {
+        json: mockRequestBody,
+    });
+    // Assertions
+    console.log(body);
+    t.is(statusCode, 200, 'Should return 200 ');
+    
+    t.truthy(body.birthday);
+    t.truthy(body.username);
+    t.truthy(body.height);
+    t.truthy(body.weight);
+    t.truthy(body.meal);
+    t.truthy(body.allergies);
+    t.truthy(body.restrictions);
+    t.truthy(body.level);
+    t.truthy(body.goal);
+    t.truthy(body.gender);
+});
 
-        // //////
+test("Post addUserDetails function returns user details", async (t) => {
+    const newUserID = 123;
+    const mockRequestBody = {   
+        userID : newUserID,
+        username : "string",
+        birthday : "2023-12-12T01:10:12.302Z",
+        gender : "string",
+        height : 0,
+        weight : 0,
+        meal : "string",
+        allergies : "string",
+        restrictions : "string",
+        level : "string",
+        goal : "string"
+      };
 
-        // test('addUserDetails returns correct response', async (t) => {
-        //     const mockRequestBody = {
-        //         "birthday": "2000-01-23T04:56:07.000+00:00",
-        //         "meal": "meal",
-        //         "allergies": "allergies",
-        //         "goal": "goal",
-        //         "gender": "gender",
-        //         "level": "level",
-        //         "weight": 1.4658129805029452,
-        //         "restrictions": "restrictions",
-        //         "userID": 0,
-        //         "username": "username",
-        //         "height": 6.027456183070403
-        //     };
+    const User = await addUserDetails(mockRequestBody);
+    t.truthy(User.birthday);
+    t.truthy(User.meal);
+    t.truthy(User.allergies);
+    t.truthy(User.goal);
+    t.truthy(User.gender);
+    t.truthy(User.level);
+    t.truthy(User.weight);
+    t.truthy(User.restrictions);
+    t.truthy(User.username);
+    t.truthy(User.height);
+});
 
-        //     const result = await addUserDetails(mockRequestBody, 123);
-        //     // Assertions
-        //     t.deepEqual(result, {
-        //         "birthday": "2000-01-23T04:56:07.000+00:00",
-        //         "meal": "meal",
-        //         "allergies": "allergies",
-        //         "goal": "goal",
-        //         "gender": "gender",
-        //         "level": "level",
-        //         "weight": 1.4658129805029452,
-        //         "restrictions": "restrictions",
-        //         "userID": 0,
-        //         "username": "username",
-        //         "height": 6.027456183070403
-        //     });
-        // });
+test("Post addUserDetails function returns correct headers", async (t) => {
+    const newUserID = 1456;
+    mockRequestBody.userID = newUserID;
+    const { headers, statusCode } = await t.context.got.post(`user/`, {
+        json: mockRequestBody,
+    });
+    // Assertions
+    t.is(statusCode, 200, 'Should return 200 ');
+    t.truthy(headers['content-type'], 'Response should have content-type header');
+});
+
+// const mockRequestBody = {
+//     userID: 12,
+//     username: "testName",
+//     birthday: '2000-01-23T04:56:07.000+00:00',
+//     gender: 'gender',
+//     height: 6.027456183070403,
+//     weight: 1.4658129805029452,
+//     meal: "testMeal",
+//     allergies: "testAllergies",
+//     restrictions: "testRestrictions",
+//     level: "testLevel",
+//     goal: "testGoal"
+// };
+const invalidMockRequestBody = {
+    userID: 12,
+    username: "testName",
+    birthday: 'sads',
+    gender: 'gender',
+    height: 6.027456183070403,
+    weight: 1.4658129805029452,
+    meal: "testMeal",
+    allergies: "testAllergies",
+    restrictions: "testRestrictions",
+    level: "testLevel",
+    goal: "testGoal"
+};
+
+test('POST addUserDetails with invalid userID returns fail response - 400 ', async (t) => {
+    const invalidUserID = 23;
+    invalidMockRequestBody.userID = invalidUserID;
+    const { body, statusCode } = await t.context.got.post(`user/`, {
+        json: invalidMockRequestBody,
+    });
+    // Assertions
+    t.is(statusCode, 400, 'Should return 400 Bad input type for user');
+    
+});
